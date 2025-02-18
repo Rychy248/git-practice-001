1d7281eb1e7b2b6f4dff8ecaa3a1eac71f774305
Ricardo Hernández <jorgeajrhh@gmail.com>
2025-02-17 19:06:07 -0600

    update(solucion2) the solution was fixed

--- solucion al problema.js
+++ solucion al problema.js
@@ -1,3 +1,17 @@
 /**
  * Esta fue una solución que se le dio al proyecto como tal
  */
+
+
+
+// 1 - REALIZAR UNA SEGUNDA RAMA QUE TENGA POR NOMBRE "HOT-FIX-2"
+// 2 - Añaidr un segundo fichero, que tenga por nombre "solucion al problema 2.js"
+// 3 - hacer merge de la rama hot-fix-2 a master
+// 4 - Eliminar la rama hot-fix-2
+
+// git status
+// git branch
+// git add .
+// git commit -m "comentario"
+// git checkout
+// git delete
